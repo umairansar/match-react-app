bc3366623266b1ff267207fa2db5d34d011757d7
umarsaeedcheema <umar.c@snoonu.com>
2025-08-07 02:08:47 +0300

    fix cors issue

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Match Maker
+
+WIP to be used for tournament generation. players creation and match score keeping
--- back_end/application/main.py
+++ back_end/application/main.py
@@ -3,13 +3,26 @@ from back_end.application.db import init_db
 from ..domain.models.user_model import User
 from ..domain.models.match_model import Match
 from ..controller.main import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+    "localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router)
 
 @app.get("/health")
 def health():
-    return "Healthy"
+    return {"Healthy"}
 
 @app.on_event("startup")
 def on_startup():
