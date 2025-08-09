99a6acea606af51175d6d868fe21603c3860798b
Umar Saeed Cheema <23100206@lums.edu.pk>
2025-08-09 16:05:23 +0300

    add department enum to user model

---
+++ back_end/domain/enums/department_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class DepartmentEnum(str, Enum):
+  TECH = "TECH"
+  PRODUCT = "PRODUCT"
+  PARTNERSHIPS = "PARTNERSHIPS"
+    
--- back_end/domain/models/user_model.py
+++ back_end/domain/models/user_model.py
@@ -1,8 +1,9 @@
 from sqlmodel import Field, SQLModel
+from ..enums.department_enum import DepartmentEnum
 
 class UserBase(SQLModel):
-    name:str
-    department:str
+    name: str
+    department: DepartmentEnum
 
 class User(UserBase, table = True):
     id : int = Field(default=None, nullable=False, primary_key=True)
