d7a549917b777f8291924a569f0732dd2fe0c5ba
Umar Saeed Cheema <23100206@lums.edu.pk>
2025-08-12 22:10:20 +0300

    add async execution

--- back_end/controller/match_controller.py
+++ back_end/controller/match_controller.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 import datetime
-from back_end.application.db import get_session
+import asyncio
+from back_end.application.db import get_session, get_async_session
 from ..domain.models.match_model import Match as MatchModel, MatchCreate, MatchBase, MatchUpdate
 from ..domain.models.user_match_model import UserMatch, UserMatchUpdate
 from .user_controller import fetch_user
@@ -48,37 +51,51 @@ def create_match(players: MatchCreate, session: Session = Depends(get_session)):
     return match_create
 
 @router.patch("/finish", response_model=MatchBase)
-def update_match(match_id : int, scores: UserMatchUpdate, session: Session = Depends(get_session)):
-    match_db = session.get(MatchModel, match_id)
+async def update_match(match_id : int, scores: UserMatchUpdate, session: AsyncSession = Depends(get_async_session)):
+    match_db = await session.get(MatchModel, match_id)
     if not match_db:
         raise HTTPException(status_code=404, detail="Match not found")
 
     # if not validate_scores(scores):
     #     raise HTTPException(status_code=400, detail="Bad Request")
-    
     user_points_dict = {s.points:s.user_id for s in scores.scores} 
     # {11:1} -> points:user_id
     points_list = sorted(user_points_dict.items(), key= lambda x:x[0], reverse = True)
-    # sequence of tuples [(11,1),(8,2)]
+    # sorted (by points) sequence of tuples [(11,1),(8,2)]
+
+    await asyncio.gather(
+        match_finish(match_db, points_list, session),
+        user_match_finish(match_id, points_list, session),
+    )
+
+    await session.commit()
+    await session.refresh(match_db)
+
+    return match_db
+
+async def match_finish(match_db, points_list, session):
     
     winner = points_list[0][1]
     loser = points_list[1][1]
+    match_db.winner = winner
+    match_db.loser = loser
+    session.add(match_db)
     
+    
+
+async def user_match_finish(match_id, points_list, session):
+
     for points, user_id in points_list:
         statement = select(UserMatch).where(
             UserMatch.user_id == user_id,
             UserMatch.match_id == match_id
         )
-        user_match = session.exec(statement).first()
+        result = await session.execute(statement)
+        user_match = result.scalars().first()
         if user_match :
             user_match.score = points
         else : 
             session.add(UserMatch(user_id = user_id, match_id = match_id, score = points))
 
-    match_db.winner = winner
-    match_db.loser = loser
-    session.add(match_db)
-    session.commit()
-    session.refresh(match_db)
-
-    return match_db
+async def user_finish():
+    pass
