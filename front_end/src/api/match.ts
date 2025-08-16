360fbb79ea1eb02ec724946d235fecb9da523583
Umar Saeed Cheema <23100206@lums.edu.pk>
2025-08-16 13:01:37 +0300

    add finish match endpoint

--- front_end/src/api/match.ts
+++ front_end/src/api/match.ts
@@ -13,6 +13,10 @@ const MatchRepository = () => ({
     createMatch: async (match: CreateMatchRequest) => {
         return await api.post<CreateMatchRequest, Match>('/match/create', match);
     }
+    , 
+    finishMatch: async (match: UpdateMatchRequest) => {    
+        return await api.patch<UpdateMatchRequest, Match>(`/match/finish?match_id=${match.match_id}`, match);
+      },
     });
 
 export default MatchRepository;
